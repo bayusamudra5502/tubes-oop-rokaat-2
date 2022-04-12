73aa59505313e82d9c94b59f9b003bb9dc35b07c
bayusamudra5502 <bayusamudra.55.02.com@gmail.com>
2022-04-12 17:51:35 +0700

    Menambah test lintas paket

---
+++ src/test/java/com/mobita/aether/test/TestCharacter.java
@@ -0,0 +1,14 @@
+package com.mobita.aether.test;
+
+import com.mobita.aether.model.Character;
+import com.mobita.aether.model.Type;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestCharacter {
+    @Test
+    public void toStringTest(){
+        Character ch = new Character("A", "B", Type.NETHER);
+        Assertions.assertEquals(ch.toString(), "Name: A\nDescription: B\nType: NETHER");
+    }
+}

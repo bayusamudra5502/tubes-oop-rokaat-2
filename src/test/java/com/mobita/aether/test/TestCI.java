be1a4605ade8eed985971195e157e0cad2e98d7b
bayusamudra5502 <bayusamudra.55.02.com@gmail.com>
2022-04-12 17:40:41 +0700

    Menambahkan CI

---
+++ src/test/java/com/mobita/aether/test/TestCI.java
@@ -0,0 +1,11 @@
+package com.mobita.aether.test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestCI {
+    @Test
+    public void testCI(){
+        Assertions.assertEquals(1+1, 2);
+    }
+}

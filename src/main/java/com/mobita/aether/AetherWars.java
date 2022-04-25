0f63f30ae21c0700a3784dd8983cd98d8568177e
bayusamudra5502 <bayusamudra.55.02.com@gmail.com>
2022-04-25 14:24:44 +0700

    Menambahkan form

--- src/main/java/com/mobita/aether/AetherWars.java
+++ src/main/java/com/mobita/aether/AetherWars.java
@@ -43,7 +43,7 @@ public class AetherWars extends Application {
 
   public void start(Stage stage) throws IOException {
     FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("form/AetherWars.fxml"));
-    Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+    Scene scene = new Scene(fxmlLoader.load(), 933, 710);
     stage.setTitle("Hello!");
     stage.setScene(scene);
     stage.show();
--- src/main/java/com/mobita/aether/form/AetherWars.java
+++ src/main/java/com/mobita/aether/form/AetherWars.java
@@ -4,11 +4,5 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class AetherWars {
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Banyak TUBES :D");
-    }
 }

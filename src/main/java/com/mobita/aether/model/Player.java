81c3f66e387c8d1f5d74aa24fcf50add22276bf7
hanafathiyah <hanafathiyah9b21@gmail.com>
2022-04-25 14:49:13 +0700

    add Player.java, not done

---
+++ src/main/java/com/mobita/aether/model/Player.java
@@ -0,0 +1,19 @@
+package com.mobita.aether.model;
+
+public class Player {
+    private String name; // name of the player
+    private String pathProfile; // profil picture of the player
+
+    public Player(String name) {
+
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getPathProfile(){
+        return this.getPathProfile();
+    }
+}

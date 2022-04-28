f57c7c2422d888aa5a8691a35eaec4267f59e572
bayusamudra5502 <bayusamudra.55.02.com@gmail.com>
2022-04-28 21:36:11 +0700

    Menjadi seorang rubah

---
+++ src/main/java/com/mobita/aether/form/DeckSelector.java
@@ -0,0 +1,21 @@
+package com.mobita.aether.form;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+
+public class DeckSelector {
+    @FXML
+    public void onCard1Clicked(MouseEvent mouseEvent){
+
+    }
+
+    @FXML
+    public void onCard2Clicked(MouseEvent mouseEvent) {
+    }
+
+    @FXML
+    public void onCard3Clicked(MouseEvent mouseEvent) {
+    }
+}

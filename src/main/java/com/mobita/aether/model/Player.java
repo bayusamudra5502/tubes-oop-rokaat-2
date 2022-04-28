ede98d3140e2c6b5db550cb9d8f3962ab1cc1ade
hanafathiyah <hanafathiyah9b21@gmail.com>
2022-04-28 23:32:23 +0700

    add player, not done

--- src/main/java/com/mobita/aether/model/Player.java
+++ src/main/java/com/mobita/aether/model/Player.java
@@ -1,13 +1,23 @@
 package com.mobita.aether.model;
 
+import com.mobita.aether.collection.Deck;
+
+import java.util.ArrayList;
+
 public class Player {
     private String name; // name of the player
     private String pathProfile; // profil picture of the player
-
+    private Integer healthPoints;
+    private Integer mana;
+    private Deck deck;
+    private Hand hand;
     public Player(String name, String pathProfile) {
-
         this.name = name;
         this.pathProfile = pathProfile;
+        this.healthPoints = 80;
+        this.mana = 0;
+        this.deck = new Deck();
+        this.hand = new Hand();
     }
 
     public String getName(){
@@ -16,5 +26,26 @@ public class Player {
 
     public String getPathProfile(){
         return this.getPathProfile();
+    }
+
+    public Card getNewCard(String slot) {
+        return this.deck.get(slot);
+    }
+
+    public String seeDescriptionHand() {
+        return "Lorem Ipsum";
+        // belum selesai
+    }
+
+    public void handToBoard(String slotHand, String slotBoard, Card card) {
+        // belum selesai
+    }
+
+    public void attackEnemy() {
+        // belum
+    }
+
+    public void nextPhase() {
+
     }
 }
